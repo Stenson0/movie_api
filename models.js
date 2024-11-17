8fa80e30b4c7fa0f47204b5bce13473c7d6035e3
Stenson0 <161175213+Stenson0@users.noreply.github.com>
2024-11-17 15:07:57 -0600

    Updated to work with fixed endpoints

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
 	Title: {type: String, required: true},
@@ -15,13 +16,21 @@ let movieSchema = mongoose.Schema({
 	Featured: Boolean
 });
 
-let userSchema = mongoose.Schema({
-	Username: {type: String, required: true},
-	Password: {type: String, required: true},
-	Email: {type: String, required: true},
-	Birthday: Date,
-	FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+let userSchema = new mongoose.Schema({
+    Username: { type: String, required: true },
+    Password: { type: String, required: true },
+    Email: { type: String, required: true },
+    Birthday: Date,
+    FavoriteMovies: [{ type: String, ref: 'Movie'}] 
 });
+
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);

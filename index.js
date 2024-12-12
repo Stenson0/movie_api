5dd0e2ba4cb44590a9b857c985e06752be3fb735
Stenson0 <161175213+Stenson0@users.noreply.github.com>
2024-12-12 00:35:10 -0600

    Connected database to API

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const express = require('express'),
 const Movies = Models.Movie;	
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB');
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(express.static('public'));

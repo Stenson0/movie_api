9944630a9d862d49294535bde94363eafcfee557
Stenson0 <161175213+Stenson0@users.noreply.github.com>
2024-12-12 16:20:05 -0600

    Port fix

--- index.js
+++ index.js
@@ -231,5 +231,5 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
- console.log('Listening on Port ' + port);
+ console.log('Listening on Port ${port}');
 });
